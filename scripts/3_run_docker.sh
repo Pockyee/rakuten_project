3667427d7d48cfaa613a8813b75698f58c4a62bb
pockyee <baoqi85@gmail.com>
2025-06-17 15:48:28 +0000

    remove redundan code

--- scripts/3_run_docker.sh
+++ scripts/3_run_docker.sh
@@ -7,13 +7,5 @@ PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 cd "$PROJECT_ROOT"
 
-RAW_DIR="./raw_data"
-if [ ! -d "$RAW_DIR" ]; then
-  echo "Creating directory: $RAW_DIR"
-  mkdir -p "$RAW_DIR"
-else
-  echo "Directory already exists: $RAW_DIR"
-fi
-
 echo "Starting Docker Compose services..."
 docker compose up -d
